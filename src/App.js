3b4623cdf8f49ecf5db7881654d52f2592e8ca04
murari9945 <kolli.pavanmurari@gmail.com>
2023-05-23 17:40:05 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+    <h1>lets get started here</h1>
+    
+    <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+function ExpenseItem(){
+    return(
+        <div>
+        <div>Expense Items</div>
+        <div>
+           <h2>food
+            </h2>
+            <h2>Rs120</h2> 
+        </div>
+        <div>
+           <h3>petrol
+            </h3>
+            <h2>Rs120</h2> 
+        </div>
+        <div>
+           <h2>pizza
+            </h2>
+            <h2>Rs120</h2> 
+        </div>
+        </div>
+    ) ;
+}
+export default ExpenseItem;
