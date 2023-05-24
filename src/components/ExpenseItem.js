d2f845da28321ef39f5320ce973de8282f267e5f
murari9945 <kolli.pavanmurari@gmail.com>
2023-05-24 14:34:27 +0530

    delete button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,22 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.js';
 
 import ExpenseDetails from './ExpenseDetails.js';
-function ExpenseItem(props){
+const ExpenseItem=(props)=>{
+    const clickHandler=()=>{
+        console.log("clicked!!!")
+    }
+    const deleteExpenseHandler = () => {
+        const expenseItem = document.querySelector('.expense-item');
+        expenseItem.remove();
+      };
     const { name, price, location } = props;
    
     return(
         <div className='expense-item'>
           <ExpenseDate date={props.date} />
         <ExpenseDetails price={props.price}  location={props.location}   name={props.name} ></ExpenseDetails>
+        <button onClick={clickHandler}>change Title</button>
+        <button onClick={deleteExpenseHandler}>Delete Expense</button>
        
        
         </div>
